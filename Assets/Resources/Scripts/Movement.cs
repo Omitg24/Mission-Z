755fbb3e988e5e79fa4540596d345a66ef485f52
Omar Teixeira González <omartg24@gmail.com>
2023-10-21 03:39:37 +0200

    21/10/2023 Finalizado mapa y creación de NPC Rajoy

--- Assets/Resources/Scripts/Movement.cs
+++ Assets/Resources/Scripts/Movement.cs
@@ -17,46 +17,35 @@ public class Movement : MonoBehaviour
     public float gravity = -9.81f;
     public float jumpHeight = 2f;
 
-    public string goal = "RabbitHole";
-    public string nextScene = "Menu";
+    public Animator anim;
 
     Vector3 velocity;
-
+   
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
+        
+        anim.SetFloat("vertical", z);
+        anim.SetFloat("horizontal", x);
 
         Vector3 move = transform.right * x + transform.forward * z;
         controller.Move(move * speed * Time.deltaTime);
 
         if (Input.GetButtonDown("Jump"))
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-
+            velocity.y = Mathf.Sqrt (jumpHeight * -2f * gravity);
+        
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
     }
 
-    public void OnControllerColliderHit(ControllerColliderHit hit)
-    {
-        if (goal != null && nextScene != null)
-        {
-            if (hit.gameObject.CompareTag(goal))
-            {
-                Debug.Log("Goal reached");
-                SceneManager.LoadScene(nextScene);
-            }
-        } else {
-            Debug.Log("Not defined");
-        }
-    }
 }
